a3f3df0a09a103ca4127d84fdc688850b2b4d5d1
Nachiket Gomkale <97365258+Snach13@users.noreply.github.com>
2022-10-03 00:00:50 +0530

    Week 6 assignments (#6)
    * assignment 1 is uploaded and under development
    
    * week 2 2nd assugnment(transition assignment)
    
    * week 3 memoization assignment completed
    
    * Adding Call, Bind, & Apply assignment
    
    * explain the output of the code
    
    * week-3:A4-Encapsulation Achieved
    
    * week-4:A4.2-output is prints out to the console
    
    * Fibonacci Series with Iterators is implemented
    
    * week-4:A1-getNumber function implemented
    
    * week-4:A4.2-Inheritance achieved
    
    * week-5:A5.1-functionality achieve(async/await)
    
    * week5:A5.2-solved using Map API
    
    * week5:A5.3-function imple using set API
    
    * week5-A5.4: adds valid input check
    
    * week5-A5.4: test cases written using jest
    
    * files added
    
    * gitignore
    
    * week-6:A1-Max sum Contiguous Subarray
    
    * Best time to buy and sell stock
    
    * Pair With Given Difference
    
    * 3 sum
    
    * Max Sum Contiguous Subarray
    
    * Spiral Order Matrix II
    
    * Sort array of 0's,1's and 2's

---
+++ Week-6/Problem 6.4/Stock.java
@@ -0,0 +1,25 @@
+public class Stock {
+
+  static int maxProfit(int prices[], int n) {
+    int buy = prices[0], max_profit = 0;
+    for (int i = 1; i < n; i++) {
+
+      // Checking for lower buy value
+      if (buy > prices[i])
+        buy = prices[i];
+
+      // Checking for higher profit
+      else if (prices[i] - buy > max_profit)
+        max_profit = prices[i] - buy;
+    }
+    return max_profit;
+  }
+
+  // Driver Code
+  public static void main(String args[]) {
+    int prices[] = { 7, 1, 5, 6, 4 };
+    int n = prices.length;
+    int max_profit = maxProfit(prices, n);
+    System.out.println(max_profit);
+  }
+}
